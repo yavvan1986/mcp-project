6bf97db50a3cc0feebae68eac227ed38225c5e0f
admin <samskrithi.b@gmail.com>
2025-10-29 12:00:28 +0000

    Added Backend Folder

---
+++ backend/src/test/java/com/abdul_yavvan/mcp/server/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul_yavvan.mcp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
